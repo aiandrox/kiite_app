3ff45472c024a9e8eb431b3e418696e7f18f9034
Aiandrox <aiandrox7@gmail.com>
2018-12-31 15:37:53 +0900

    Create new kiite form and action

--- app/controllers/kiites_controller.rb
+++ app/controllers/kiites_controller.rb
@@ -4,8 +4,21 @@ class KiitesController < ApplicationController
   end
 
   def new
+    @kiite = Kiite.new
+  end
+
+  def create
+    kiite = Kiite.new(kiite_params)
+    kiite.save
+    redirect_to root_path, notice: "投稿しました"
   end
 
   def edit
   end
+
+  private
+  def kiite_params
+    params.require(:kiite).permit(:text, :sokka)
+  end
+  
 end
