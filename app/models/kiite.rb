c508367edacb0966d94b2e0111a81cea5da6e21f
Aiandrox <aiandrox7@gmail.com>
2018-12-31 16:05:26 +0900

    annotate

--- app/models/kiite.rb
+++ app/models/kiite.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: kiites
+#
+#  id         :integer          not null, primary key
+#  text       :text
+#  sokka      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Kiite < ApplicationRecord
 end
--- test/models/kiite_test.rb
+++ test/models/kiite_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: kiites
+#
+#  id         :integer          not null, primary key
+#  text       :text
+#  sokka      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class KiiteTest < ActiveSupport::TestCase
